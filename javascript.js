97e7d11ab0fb08ed9dce5371b06d9885f95f2aa4
Simon123457123 <simon021663@gmail.com>
2026-09-22 13:51:54 +0200

    Nyt spil og gæt virker.

--- javascript.js
+++ javascript.js
@@ -3,21 +3,18 @@ const visHint = document.getElementById("hintText")
 const skriveFeltet = document.getElementById("skrivefelt")
 const tælGæt = document.getElementById("gætTæller");
 const AntalGæt = document.getElementById("antalAfGæt");
+const startSpil = document.getElementById("nytSpil");
+const skrivHint = document.getElementById("hintKnap")
+const tjekBy = document.getElementById("sendSvar");
 
 const byer = [
-  { byNavn: "Nakskov", indbyggertal: "12.000", førsteBogstav: "N" },
-  { byNavn: "Maribo", indbyggertal: "5.800", førsteBogstav: "M" },
-  { byNavn: "Sakskøbing", indbyggertal: "4.500", førsteBogstav: "S" }]
-
-
-/*  document.getElementById("hintKnap").addEventListener("click", function () {
- let byerTest = byer.map(by => by.byNavn).join(", ")
- console.log(byerTest);
-});
-*/
+  { byNavn: "Nakskov", indbyggertal: "12000", førsteBogstav: "N" },
+  { byNavn: "Maribo", indbyggertal: "5800", førsteBogstav: "M" },
+  { byNavn: "Sakskøbing", indbyggertal: "4500", førsteBogstav: "S" }]
 
 const by = []
 let tæller = 1;
+
 function By(skriveFeltet) {
   this.skriveFeltet = skriveFeltet;
   this.toString = function () {
@@ -25,7 +22,51 @@ function By(skriveFeltet) {
   }
 }
 
-function visListe() {
+function getRandomItem(arr) {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
+}
+
+
+startSpil.addEventListener("click", () => {
+  currentCity = getRandomItem(byer)
+  console.log(currentCity.byNavn)
+  // if (currentCity.byNavn === skriveFeltet.value) {
+  //   alert("Du har gættet rigtigt")
+  //   console.log()
+  // } else {
+  //   alert("Forkert")
+  // }
+});
+
+skrivHint.addEventListener("click", () => {
+  console.log();
+});
+
+tjekBy.addEventListener("click", () => {
+  AntalGæt.textContent = "Du har brugt " + tæller++ + " gæt";
+  if (currentCity.byNavn === skriveFeltet.value) {
+    alert("Du har gættet rigtigt")
+    console.log()
+  } else {
+    alert("Forkert")
+  }
+});
+
+
+
+/*document.getElementById("sendSvar").addEventListener("click", function () {
+  if (!skriveFeltet.value) alert("Du mangler at indtaste en film!");
+  else {
+    by.push(new By(skriveFeltet.value));
+    visListe();
+    tælGæt.style.textAlign = "center";
+
+  }
+});
+*/
+
+/*function visListe() {
   AntalGæt.textContent = "";
   for (let i = 0; i < by.length; ++i) {
     const p = document.createElement("p");
@@ -33,15 +74,30 @@ function visListe() {
     p.textContent = by[i].toString();
     p.appendChild(span);
     AntalGæt.appendChild(p);
-
   }
 }
+*/
 
-document.getElementById("sendSvar").addEventListener("click", function () {
-  if (!skriveFeltet.value) alert("Du mangler at indtaste en film!");
-  else {
-    by.push(new By(skriveFeltet.value));
-    visListe();
-    tælGæt.textContent = "Dine antal gæt er " + tæller++;
-  }
-});
+// const cars = ["BMW", "Volvo", "Saab", "Ford"];
+// let len = cars.length;
+
+// let text = "";
+// for (let i = 0; i < len; i++) {
+//   text += cars[i];
+// }
+// console.log(cars)
+
+// let leng = byer.length;
+
+// let textt = "";
+// for (let i = 0; i < leng; i++) {
+//   textt += cars[i];
+// }
+// console.log(byer)
+
+
+// let items = Array("test", "test2", "q", "a");
+// var item = items[Math.floor(Math.random() * items.length)];
+// console.log(item);
+
+
